56a5eb84fc5713e83af8a99bff8e93f52fbafb5e
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2021-01-29 17:16:02 +0100

    Add psql usage

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"log"
+	"database/sql"
+	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -29,9 +31,34 @@ func main() {
 	articles = append(articles, Article{ID: "1", Title: "My first article", Content: "This is the content of the first article", Author: Author{ID: "1", Name: "Jhon Doe"}})
 
 	err := godotenv.Load(".env")
+	CheckError(err)
 
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+
+}
+
+// CheckError stop the program if an error is detected
+func CheckError(err error) {
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		panic(err)
 	}
-
 }
